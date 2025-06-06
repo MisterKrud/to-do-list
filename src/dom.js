0fc859083502ec648d7ebd6ebb7010ed0697068a
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-06-07 09:59:58 +1000

    Add styling to item card
    Category moved to top smaller font greyed
    Notes - smaller font - italic
    Border-radius added
    BAckgroiund color added

--- src/dom.js
+++ src/dom.js
@@ -10,11 +10,13 @@ content.appendChild(itemContainer);
 
 console.log(`items in dom.js: ${items}`)
     items.forEach(item => {
+        const completeClass = () =>item.complete ? "complete" : "incomplete";
         console.log(`item.title: ${item.title}`)
         const itemCard = document.createElement("div");
         itemCard.setAttribute("class", "item-card");
         itemCard.setAttribute("id", item.id);
         const title = document.createElement("h4")
+        title.setAttribute("class", "item-header")
         title.textContent = item.title;
         const itemContent = document.createElement("div")
         itemContent.setAttribute("class", "item-content")
@@ -25,15 +27,18 @@ console.log(`items in dom.js: ${items}`)
         const priority = document.createElement("p");
         priority.textContent = item.priority;
         const notes = document.createElement("p");
-        priority.textContent = item.priority;
+        notes.setAttribute("class", "notes")
+        notes.textContent = item.notes;
         const complete = document.createElement("p");
-        complete.setAttribute("class", () => item.complete ? "complete" : "incomplete");
+        complete.setAttribute("class", completeClass());
+        complete.innerText = (completeClass());
         const category = document.createElement("p");
+        category.setAttribute("class", "category")
         category.textContent = item.category;
         itemContainer.appendChild(itemCard);
         // itemCard.appendChild(title)
-        itemCard.append(title,itemContent)
-        itemContent.append(description,dueDate,notes,priority,complete,category)
+        itemCard.append(category, title,itemContent)
+        itemContent.append(description,dueDate,notes,priority,complete,)
     }
     )
 
