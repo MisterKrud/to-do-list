8bda0d8248d2e0df16e049f81a06e5dad4b018e9
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-06-08 14:51:51 +1000

    Begin adding project actions to DOM
    Very little so far. All I've managed to do
    is set the class of the chosen project to 'active'
    
    Not much progress otherwise

--- src/tasksHeader.js
+++ src/tasksHeader.js
@@ -1,19 +1,39 @@
 import { items } from "./manageItems";
-import { categories } from "./manageCategories";
+import { categories, filter } from "./manageCategories";
+
+
 
 const tasksHeader = () => {
+    const content = document.getElementById("content")
     const header = document.querySelector("header")
     const projectsContainer = document.createElement("div")//.id("projects");
     const projectsList = document.createElement("ul")//.id("projects-list");
+    const mainScreen = document.getElementById("item-container")
 
     header.appendChild(projectsContainer)
     projectsContainer.appendChild(projectsList)
 
     categories.forEach(category => {
-        const project = document.createElement("li")//.className("project-name")
+        const project = document.createElement("li")
+        // project.className("project-name");
         project.textContent = category;
         projectsList.appendChild(project);
+
+       project.addEventListener("click", () =>{
+           const projectNames = document.querySelectorAll(".project-name");
+           projectNames.forEach(projName => projName.classList.remove("active"));
+            project.classList.add("active");
+            
+            
+
+        })
     })
+
+
+
+
+
+
 
 console.table(categories)
 
