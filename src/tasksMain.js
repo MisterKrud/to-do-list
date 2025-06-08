402b8248cf4eadadea29215b78bd5e8e1ea0a582
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-06-08 14:14:40 +1000

    Move completed items to bottom of list in dom
    Also grey them out and strike through the text

--- src/tasksMain.js
+++ src/tasksMain.js
@@ -2,127 +2,120 @@ import { items } from "./manageItems";
 import { toggleComplete } from "./manageItems";
 
 const tasksMain = () => {
-const content = document.getElementById("content");
-const itemContainer = document.createElement("div");
-itemContainer.setAttribute("id", "item-container")
-content.appendChild(itemContainer);
+  const content = document.getElementById("content");
+  const itemContainer = document.createElement("div");
+  itemContainer.setAttribute("id", "item-container");
+  content.appendChild(itemContainer);
 
- 
+  console.log(`items in dom.js: ${items}`);
+  items.forEach((item) => {
+    const completeClass = () => (item.complete ? "complete" : "incomplete");
 
-console.log(`items in dom.js: ${items}`)
-    items.forEach(item => {
-       const completeClass = () =>item.complete ? "complete" : "incomplete";
-   
-  
-    
-        console.log(`item.title: ${item.title}`)
-        const itemListView = document.createElement("div");
-        itemListView.setAttribute("class", "item-list-view");
-        itemListView.setAttribute("id", item.id);
-        const title = document.createElement("p")
-        title.setAttribute("class", "item-header")
+    console.log(`item.title: ${item.title}`);
+    const itemListView = document.createElement("div");
+        itemListView.className="item-list-view";
+        itemListView.id =item.id;
+    const title = document.createElement("p");
+        title.className ="item-header";
         title.textContent = item.title;
-        const itemContent = document.createElement("div")
-        itemContent.setAttribute("class", "item-content")
-        const description = document.createElement("p")
+    const itemContent = document.createElement("div");
+        itemContent.className = "item-content";
+    const description = document.createElement("p");
         description.textContent = item.description;
-        const dueDate = document.createElement("p")
-        dueDate.setAttribute("class", "due-date")
+    const dueDate = document.createElement("p");
+        dueDate.className = "due-date";
         dueDate.textContent = `  | ${item.dueDate}`;
-        const priority = document.createElement("p");
+    const priority = document.createElement("p");
         priority.textContent = item.priority;
-        const notes = document.createElement("p");
-        notes.setAttribute("class", "notes")
+    const notes = document.createElement("p");
+        notes.className = "notes";
         notes.textContent = item.notes;
-        const complete = document.createElement("p");
-        complete.setAttribute("class", completeClass());
-        complete.innerText = (completeClass());
-        const category = document.createElement("p");
-        category.setAttribute("class", "category")
+    const complete = document.createElement("p");
+        complete.className = completeClass();
+        complete.innerText = completeClass();
+    const category = document.createElement("p");
+        category.className = "category";
         category.textContent = item.category;
-        itemContainer.appendChild(itemListView);
+    
+    
 
-        const checkBox = document.createElement("input");
-        
-      checkBox.setAttribute("type", "checkbox")
-      checkBox.setAttribute("class", "check-box")
-checkBox.classList.add(completeClass())
-       
-        
+    const checkBox = document.createElement("input");
 
-        // itemCard.appendChild(title)
-        itemListView.append(checkBox, title, itemContent );
-       itemContent.append(category, dueDate)
-    }
-    )
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("class", "check-box");
+    checkBox.classList.add(completeClass());
 
-    const checkBoxes = document.querySelectorAll(".check-box");
-    // console.table(checkBoxes)
-
-        checkBoxes.forEach(box => {
-           
-            box.addEventListener("click", () =>{
-             
-              const boxParentId = box.parentElement.id
-              console.log(box.parentElement)
-              console.log(box.parentElement.id)
-             const idIndex = items.findIndex(item => item.id === boxParentId)
-               
-                if (box.getAttribute("class") === 'complete'){
-                    box.classList.add('incomplete')
-                    box.classList.remove('complete')
-                   
-                    
-                    
-                } else {
-                    box.classList.add('complete');
-                    box.classList.remove('incomplete');
-                    
-                    
-                }
-            
-                console.log(items[idIndex])
-                console.log(items[idIndex].complete)
-                toggleComplete(items[idIndex])
-                 console.log(items[idIndex].complete)
-                console.table(items)
-            } )
-        })
+    itemContainer.appendChild(itemListView);
+        itemListView.append(checkBox, title, itemContent);
+            itemContent.append(category, dueDate);
+  });
 
 
     const itemListView = document.querySelectorAll(".item-list-view");
-    itemListView.forEach(item =>{
+  itemListView.forEach((item) => {
+    item.addEventListener("click", () => {
+      itemListView.forEach((listItem) => listItem.classList.remove("selected"));
+      console.log(item.firstChild.className)
+      if (item.firstChild.className ==="check-box complete"){
+        return} else {
+        item.classList.add("selected");
+      }
+    });
+  });
+
+  const checkBoxes = document.querySelectorAll(".check-box");
+  // console.table(checkBoxes)
+
+  checkBoxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      const boxParentId = box.parentElement.id;
+      console.log(box.parentElement);
+      console.log(box.parentElement.id);
+      const idIndex = items.findIndex((item) => item.id === boxParentId);
+
+      if (box.getAttribute("class") === "complete") {
+        box.classList.add("incomplete");
+        box.classList.remove("complete");
+      } else {
+        box.classList.add("complete");
+        box.classList.remove("incomplete");
+        const completedItemToShift = document.getElementById(boxParentId);
+        completedItemToShift.classList.remove("selected");
+        itemContainer.removeChild(completedItemToShift);
+        itemContainer.appendChild(completedItemToShift);
+        completedItemToShift.removeChild(completedItemToShift.querySelector("div"));
         
-        item.addEventListener("click", ()=>{
-            itemListView.forEach(listItem => listItem.classList.remove("selected"))
-            item.classList.add("selected")
-          
-        })
-    })
+        
+      }
 
+     
+      toggleComplete(items[idIndex]);
   
-    // const checkBoxes = document.querySelectorAll(".check-box");
+    });
+  });
 
- 
-    //     checkBoxes.forEach(box => {
-    //         box.addEventListener("click", () =>{
-    //             console.log(box.getAttribute("class"))
-    //             if (box.getAttribute("class") === 'complete'){
-    //                 box.classList.add('incomplete')
-    //                 box.classList.remove('complete')
-                    
-    //             } else {
-    //                 box.classList.add('complete');
-    //                 box.classList.remove('incomplete');
-    //             }
-    //         } )
-    //     })
-   
 
-return itemContainer;
-}
+
+
+  // const checkBoxes = document.querySelectorAll(".check-box");
+
+  //     checkBoxes.forEach(box => {
+  //         box.addEventListener("click", () =>{
+  //             console.log(box.getAttribute("class"))
+  //             if (box.getAttribute("class") === 'complete'){
+  //                 box.classList.add('incomplete')
+  //                 box.classList.remove('complete')
+
+  //             } else {
+  //                 box.classList.add('complete');
+  //                 box.classList.remove('incomplete');
+  //             }
+  //         } )
+  //     })
+
+  return itemContainer;
+};
 
 // page()
 
-
-export { tasksMain }
+export { tasksMain };
